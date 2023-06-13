db448e777bf4d7bf2f2b9581aea0ff598494f3ce
qstoker <quinnpstoker@gmail.com>
2023-06-12 20:29:08 -0700

    finished program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2005;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2005;
+        job2._endYear = 2010;
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResumeDetails();
     }
 }
