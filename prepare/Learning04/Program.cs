492d49b42ebd7039e4132650d683be62c82a8dcd
qstoker <quinnpstoker@gmail.com>
2023-06-27 11:23:20 -0700

    completed assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment multiplication = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(multiplication.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment fractions = new MathAssignment(
+            "Roberto Rodriguez",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+        Console.WriteLine(fractions.GetSummary());
+        Console.WriteLine(fractions.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment europeanHistory = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+        Console.WriteLine(europeanHistory.GetSummary());
+        Console.WriteLine(europeanHistory.GetWritingInformation());
     }
 }
