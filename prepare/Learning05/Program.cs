cdedf4c8cf9c51b07320022ae7ef7aa0167d73dc
qstoker <quinnpstoker@gmail.com>
2023-07-02 14:38:30 -0700

    Finished assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(new Square("blue", 3));
+        shapeList.Add(new Rectangle("red", 5, 2));
+        shapeList.Add(new Circle("yellow", 7.1));
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} - Area: {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/Rectangle.cs
@@ -0,0 +1,17 @@
+public class Rectangle : Shape
+{
+    double _length;
+    double _width;
+
+    public Rectangle(string color, double length, double width)
+        : base(color)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
